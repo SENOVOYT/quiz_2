63168726e586e0c86b97aa9a1c3479528b8dc654
Driane Diojan Perez <drianeperez12@gmail.com>
2022-10-04 22:47:59 -0600

    star click

--- Star/src/Star.elm
+++ Star/src/Star.elm
@@ -1,12 +1,49 @@
+--MVU architecture (elm aarchitecture)
+--model
+--model / state
 module Star exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing ( class, src)
-main : Html msg
-main =
+import Html.Events exposing (onClick)
+import Browser
+type Msg =
+    Like | Unlike
+initialModel : { liked : Bool }
+initialModel = 
+    {
+        liked = True
+    }
+viewstar : { liked : Bool } -> Html Msg
+viewstar model =
+    let
+        buttonType = 
+            if model.liked then "star" else "stars"
+        msg =
+            if model.liked then Unlike else Like
+    in
+
     div [class "header"][
-    span [ class "material-symbols-outlined" ] [ text "star" ] ]
+    span [ class "material-symbols-outlined", onClick msg ] [ text buttonType ] ]
+view : { liked : Bool } -> Html Msg
+view model =
+    viewstar model
+update : Msg -> { liked : Bool } -> { liked : Bool }
+update msg model =
+    case msg of
+        Like ->
+            { model | liked = True }
+        Unlike ->
+            { model | liked = False }
 
-
+main : Program () { liked : Bool } Msg
+main =
+    Browser.sandbox
+    {
+        init = initialModel
+        ,view = view
+        ,update = update
+    }
+        
 --     <span class="material-symbols-outlined">
 -- star
 -- </span>
