727e4c686a1e38a4c4088c6d2c0ecc3845a9e213
Driane Diojan Perez <drianeperez12@gmail.com>
2022-10-04 20:50:57 -0600

    elm project second step

--- Star/src/Star.elm
+++ Star/src/Star.elm
@@ -1,5 +1,7 @@
 module Star exposing (main)
-import Html exposing (Html, div, text)
+import Html exposing (h1, Html, div, text)
+import Html.Attributes exposing ( class )
 main : Html msg
 main =
-    div [] [ text "star" ]
+    div [ class "header" ] 
+    [ h1 [] [ text "Star" ] ]
